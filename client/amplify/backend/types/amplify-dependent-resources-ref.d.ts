70e55f2429843c5059ff744ee58da5f0b606c1b7
ragulsuryas <ragulsurya369@gmail.com>
2024-06-10 16:55:33 +0530

    auth added

---
+++ client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "imdbclientbf80e9ea": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,20 +6,31 @@ import MovieDetails from "./pages/MovieDetails";
 import Watchlist from "./pages/Watchlist";
 import { WatchlistProvider } from "./context/WatchlistContext";
 
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import "./App.css"
+//@ts-ignore
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
+
 const App = () => {
   return (
-    <WatchlistProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<h1>Page Not Found!</h1>} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-          <Route path="/watchlist" element={<Watchlist />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </WatchlistProvider>
+    <Authenticator>
+      <WatchlistProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<h1>Page Not Found!</h1>} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </WatchlistProvider>
+    </Authenticator>
   );
 };
 
