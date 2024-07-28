6a70aa7a9c1e01bc0ac6b0d76d5d62ac237281c0
ragulsuryas <ragulsurya369@gmail.com>
2024-07-28 23:19:17 +0530

    url alter

--- client/src/components/Actor/ActorCard.tsx
+++ client/src/components/Actor/ActorCard.tsx
@@ -1,13 +1,12 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import { api_url } from "../../context/WatchlistContext";
 
 const ActorCard: FC<{ url: string; name: string; actorId: string, page: boolean }> = ({ url, name, actorId, page }) => {
   const actorImageUrl = `https://movie-assets.s3.amazonaws.com/${url}`;
 
   return (
     <div className="flex flex-col items-center gap-2">
-      <Link to={`${api_url}/actor/${actorId}`}>
+      <Link to={`https://main.d2cqlvazj6nhr8.amplifyapp.com/actor/${actorId}`}>
         <img
           src={actorImageUrl}
           className={`${page ? 'rounded-md h-[70px] w-[70px] lg:w-[200px] lg:h-[200px]' : 'rounded-full w-[70px] h-[70px] lg:w-[170px] lg:h-[170px]'} object-cover  object-top`}
